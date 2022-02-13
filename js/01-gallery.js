cfe56b213e78fca2eabb7491aefe64685f25c4d7
Tanya <duka.tanik@gmail.com>
2022-02-13 18:06:14 +0200

    change

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -21,20 +21,20 @@ const createGallery = galleryItems => {
 galleryBox.insertAdjacentHTML('beforeend', createGallery(galleryItems));
 
 
-galleryBox.addEventListener('click', onClickGalleryItem);
+ galleryBox.addEventListener('click', onClickGalleryItem);
 
-
+ let instance = {};
 function  createInstance (img) {
       instance = basicLightbox.create( 
         `<img src="${img}"> `,
         {
             onShow: () => {
                 
-                document.addEventListener(('keydown'), escClose)
+                document.addEventListener(('keydown'), onEscPress)
             },
             onClose: () => {
                 
-                document.removeEventListener(('keydown'), escClose)
+                document.removeEventListener(('keydown'), onEscPress)
             }
         });
     
@@ -62,4 +62,56 @@ function onEscPress  (event)  {
 
 
 
+// const gallery = document.querySelector('.gallery');
 
+
+// function createGallaryCards(images) {
+//     return images.map(({preview, original, description }) => {
+//         return `<div class="gallery__item">
+//         <a href="${original}" class="gallery__link">
+//         <img
+//         class="gallery__image"
+//         src="${preview}" 
+//         data-source="${original}"
+//         alt="${description}">        
+//         </a>
+//         </div>`;
+//     })
+//         .join(' '); 
+// }
+
+// const cards = createGallaryCards(galleryItems);
+// gallery.innerHTML = cards;
+
+// let instance = {};
+
+// gallery.addEventListener('click', (evt) => {
+//     evt.preventDefault();
+//     const img = evt.target.dataset.source;        
+//     createInstance(img);    
+// })
+  
+// function createInstance(img) {
+//      instance = basicLightbox.create(`
+//             <img src="${img}">`,
+//         {
+//             onShow: () => {
+                
+//                 document.addEventListener(('keydown'), escClose)
+//             },
+//             onClose: () => {
+                
+//                 document.removeEventListener(('keydown'), escClose)
+//             }
+//         });
+    
+//     instance.show();
+// }
+
+// function escClose(evt) {
+    
+//     if (evt.code === 'Escape') {
+        
+//         return instance.close();        
+//         }
+//     }
